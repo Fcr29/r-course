0966193a206aec56d44296cf708d169efd931ae9
Freddy <tux@Babushka.domain.name>
2023-07-22 07:59:48 -0600

    Transformado en proyecto para vincular con github.

--- scripts/tema1/01-analisis-csv.R
+++ scripts/tema1/01-analisis-csv.R
@@ -1,20 +1,19 @@
-auto <- read.csv("../data/tema1/auto-mpg.csv", 
+auto <- read.csv("/home/tux/codigoAnacondaMLPythonR/r-course/data/tema1/auto-mpg.csv", 
                  header = TRUE, sep = ",",
-                 strings.na = "",
                  stringsAsFactors = FALSE)
 names(auto)
 
 #read.csv2 == read.csv("filename", sep = ";", dec = ",")
 # sep = "\t" (¡¡¡En el vídeo me olvidé la barra!!!)
 
-auto_no_header <- read.csv("../data/tema1/auto-mpg-noheader.csv", header = FALSE)
+auto_no_header <- read.csv("/home/tux/codigoAnacondaMLPythonR/r-course/data/tema1/auto-mpg-noheader.csv", header = FALSE)
 head(auto_no_header, 4)
 
-auto_no_sense <- read.csv("../data/tema1/auto-mpg-noheader.csv")
+auto_no_sense <- read.csv("/home/tux/codigoAnacondaMLPythonR/r-course/data/tema1/auto-mpg-noheader.csv")
 head(auto_no_sense, 4)
 
 auto_custom_header <- 
-  read.csv("../data/tema1/auto-mpg-noheader.csv",
+  read.csv("/home/tux/codigoAnacondaMLPythonR/r-course/data/tema1/auto-mpg-noheader.csv",
            header = F, 
            col.names = c("numero", "millas_por_galeon", "cilindrada", 
                          "desplazamiento", "caballos_de_potencia", 
